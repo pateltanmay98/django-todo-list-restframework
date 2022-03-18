43adfcde9b877b1b9f022fb39c4c77d9740d2fce
unknown <patelth1998@gmail.com>
2022-03-18 17:24:05 +0530

    18 03 2022

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
 # Create your views here.
 
@@ -16,3 +18,28 @@ def apiOverview(request):
         
     }
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many = False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task,data=request.data)
+    return Response(serializer.data)
