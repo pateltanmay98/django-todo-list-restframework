f5e536c9b9f7d53a01dd1c52a8743496953040d4
unknown <patelth1998@gmail.com>
2022-03-21 21:01:19 +0530

    21 03 2022

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,39 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      todoList:[],
+      activeItem:{
+        id:null,
+        title:'',
+        completed:false,
+      },
+      editing:false,
+    }
+    this.fetchTasks = this.fetchTasks.bind(this)
+  };
+
+  componentWillMount(){
+    this.fetchTasks()
+  }
+
+  fetchTasks(){
+    console.log('Fetching..')
+
+    fetch('http://127.0.0.1:8000/api/task-list')
+    .then(response => response.json())
+    .then(data => 
+      this.setState({
+        todoList: data
+      })
+    )
+  }
+
   render(){
+    var tasks = this.state.todoList
     return(
       <div className='container'>
         <div id='task-container'>
@@ -20,7 +52,9 @@ class App extends React.Component {
             </form>
           </div>
           <div id='list-wrapper'>
-            
+            {tasks.map(function(task, index){
+
+            })}
           </div>
         </div>
       </div>
